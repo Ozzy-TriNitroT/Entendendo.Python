bd7eabe11a2fd90ba132b7ac89305c84bd03c3ea
Ozzy-TriNitroT <osmardesenhos.neto@gmail.com>
2025-06-09 10:45:01 -0300

    Desafios de variáveis resolvidos

---
+++ fundamentos/01_variaveis.py
@@ -0,0 +1,15 @@
+#Variáveis básicas
+nome = "Osmar"
+idade = 21
+profissao = "Operador de Processos Químicos e Estudante de tecnologia."
+print (f"Meu nome é {nome} e tenho {idade} anos de idade e sou {profissao}.")
+
+#Operações Numericas com variáveis
+numero = 10
+resultado = numero * 2
+
+print ("O dorbro de", numero, "é", resultado)
+
+#Tipo
+mensalidade_ativa = True
+print (type(mensalidade_ativa))
